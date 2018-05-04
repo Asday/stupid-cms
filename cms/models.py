634a0d08fafeee963bcdd709b22e57132a538827
asday <sara.and.zuka@gmail.com>
2018-05-04 05:16:55 +0100

    CMS model overhaul.
    Already.

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.text import slugify
@@ -6,11 +8,8 @@ from polymorphic.models import PolymorphicModel
 
 
 """
-Sites are the top level object.  They have attributes that one would
- reasonably expect of a Site, such as a name.
-
 Pages are ordered in a hierarchy.  Each Page exists as a child of
- either another Page, or a Site.
+ either another Page, or as a top level Page.
 
 Pages contain Blocks, which can be of many types.
 
@@ -18,40 +17,79 @@ Blocks can contain References, which refer to a Page or Block.
 """
 
 
-class Site(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-
-    @property
-    def children(self):
-        return self._children.filter(parent_page=None)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name, allow_unicode=True)
-
-        return super().save(*args, **kwargs)
-
-
 class Page(models.Model):
-    parent_page = models.ForeignKey(
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+
+    parent = models.ForeignKey(
         'self',
         related_name='children',
         on_delete=models.PROTECT,
         null=True,
     )
-    site = models.ForeignKey(
-        'cms.Site',
-        related_name='_children',
-        on_delete=models.PROTECT,
+    denormalised_path = models.TextField(
+        help_text=(
+            'Ordered slash-separated list of parent `Page`s\' slugs,'
+            ' starting from the top level `Page`.  This does not'
+            ' include the current `Page`.'
+        ),
+        editable=False,
     )
 
     title = models.CharField(max_length=1024)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField()
 
-    def save(self, *args, **kwargs):
+    class Meta:
+        unique_together = ('denormalised_path', 'slug')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._old_slug = self.slug
+
+    def _denormalise_path(self):
+        # Update own `denormalised_path`
+        if self.parent:
+            # Trigger parent's path denormalisation if needed.
+            self.parent.save()
+
+            self.denormalised_path = '/'.join([
+                self.parent.denormalised_path,
+                self.parent.slug,
+            ])
+
+        else:
+            self.denormalised_path = ''
+
+        self._redenormalise_children_paths()
+
+    def _redenormalise_children_paths(self):
+        for child in self.children:
+            child.save(redenormalise_path=True)
+
+    def _redenormalise_path_if_needed(self, force=False):
+        redenormalise_path = force
+
+        # Do we need to initialise the denormalised path?
+        if not self.denormalised_path and self.parent is not None:
+            redenormalise_path = True
+
+        # If we've just been moved to the top level, we'll need to
+        # blank `denormalised_path` and update our children anyway.
+        if self.denormalised_path and self.parent is None:
+            redenormalise_path = True
+
+        # If our slug has changed...
+        if self._old_slug != self.slug:
+            redenormalise_path = True
+
+        if redenormalise_path:
+            self._denormalise_path()
+
+    def save(self, *args, redenormalise_path=False, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name, allow_unicode=True)
+
+        self._redenormalise_path_if_needed(force=redenormalise_path)
 
         return super().save(*args, **kwargs)
 
