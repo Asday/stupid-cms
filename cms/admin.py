5c57dfe71ace3919f6517bfeb1afca32b6effd8e
asday <sara.and.zuka@gmail.com>
2018-05-15 23:57:51 +0100

    Added an admin for blocks.
    Not super sure it works right, but the admin is to be frontend-loaded
    anyway so whatever.

--- cms/admin.py
+++ cms/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import Page
+from polymorphic.admin import (
+    PolymorphicChildModelAdmin,
+    PolymorphicParentModelAdmin,
+)
+
+from .models import Block, Page, TextBlock
 
 
+@admin.register(Block)
+class BlockAdmin(PolymorphicParentModelAdmin):
+    base_model = Block
+    child_models = (
+        TextBlock,
+    )
+
+
+admin.site.register(TextBlock)
 admin.site.register(Page)
