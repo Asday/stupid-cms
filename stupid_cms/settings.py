2445af42fa27442ea1a7ba3475a8aed20173f73c
asday <sara.and.zuka@gmail.com>
2018-05-15 23:41:58 +0100

    Fleshed out the frontend a bit.

--- stupid_cms/settings.py
+++ stupid_cms/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'stupid_cms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
